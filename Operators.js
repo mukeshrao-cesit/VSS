b0062b955cefbac3a64d07e33e7bf87a886c6730
mukeshrao-cesit <110102272+mukeshrao-cesit@users.noreply.github.com>
2022-10-20 19:59:51 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,48 @@
+let a = 10;
+let b = 20;
+
+// Arithmetic Operator
+// Additional Operator
+let c = a + b;
+
+// Substract Operator
+let d = a - b;
+
+// Multiplecation Operator
+let e = a * b;
+
+// Divide Operator
+let f = a / b;
+
+// Modulo Operator
+let g = a % b;
+
+// Logical Operator
+// AND Operator
+let h = a == 10 && b == 20;
+
+// OR Operator
+let i = a == 10 || b == 20;
+
+// NOT Operator
+let j = !(a == b);
+
+//Comparision Operator
+// Double Equal
+let k = a == b;
+
+//Triple Equal
+let l = a === b;
+
+// Not Equal
+let m = a != b;
+
+//Assignment Operator
+// Equal to
+let n = a;
+
+// Increment and Equal to
+a += 10;
+
+// Decrement and Equal to
+b -= 10;
