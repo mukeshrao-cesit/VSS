81fcc3aa22fda17d973569000317844891f2e3bd
mukeshrao-cesit <110102272+mukeshrao-cesit@users.noreply.github.com>
2022-11-03 20:59:08 +0530

    Add files via upload

--- Problems.js
+++ Problems.js
@@ -1,34 +1,60 @@
-let employees = [
+const users = [
   {
-    id: 11,
-    name: "Abhinav",
-    salary: 75000,
+    id: 1,
+    name: "Ankit Jain",
   },
   {
-    id: 2131,
-    name: "Raj",
-    salary: 62000,
+    id: 2,
+    name: "Shankar Sahu",
   },
   {
-    id: 3012,
-    name: "Raj",
-    salary: 32000,
+    id: 3,
+    name: "Akash Kushwaha",
   },
 ];
 
-function solve(array) {
-  let result = [];
-  for (let i in array) {
-    array[i].salary > 60000 ? result.push(array[i]) : undefined;
+const orders = [
+  {
+    orderId: 101,
+    userId: 1,
+    productId: 123,
+    productName: "Apple iPhone 14 Pro Max",
+  },
+  {
+    orderId: 102,
+    userId: 1,
+    productId: 124,
+    productName: "Apple MacBook Pro",
+  },
+  {
+    orderId: 103,
+    userId: 1,
+    productId: 125,
+    productName: "Apple iWatch 7 series",
+  },
+  {
+    orderId: 104,
+    userId: 2,
+    productId: 126,
+    productName: "Samsung S22",
+  },
+];
+const final = [];
+function solve() {
+  for (let i of users) {
+    let orderList = [];
+    for (let j of orders) {
+      if (i.id === j.userId) {
+        orderList.push({
+          orderId: j.orderId,
+          productId: j.productId,
+          productName: j.productName,
+        });
+      }
+    }
+    final.push({ ...i, orders: orderList });
   }
-  return result;
+  console.log(final);
 }
 
-console.log(solve(employees));
-
-function result(array) {
-  let resultArray = array.filter((item) => item.salary > 60000);
-  return resultArray;
-}
-
-console.log(result(employees));
+solve();
