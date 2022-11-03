cdfbb082e135d9e4cca1c9b0407d46bb615fe76b
mukeshrao-cesit <110102272+mukeshrao-cesit@users.noreply.github.com>
2022-11-03 14:22:22 +0530

    Add files via upload

--- Occurrence_Of_String.js
+++ Occurrence_Of_String.js
@@ -6,4 +6,28 @@ function occurrence(str) {
   return obj;
 }
 
+function occurrence(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (obj.hasOwnProperty(str.charAt(i))) {
+      obj[str.charAt(i)]++;
+    } else {
+      obj[str.charAt(i)] = 1;
+    }
+  }
+  return obj;
+}
+
+function occurrence(str) {
+  let obj = {};
+  for (let i in str) {
+    if (obj.hasOwnProperty(str.charAt(i))) {
+      obj[str.charAt(i)]++;
+    } else {
+      obj[str.charAt(i)] = 1;
+    }
+  }
+  return obj;
+}
+
 console.log(occurrence("malayalam"));
